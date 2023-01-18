c9bef08a38bcf2700c22a17ad5b25d2a6e85bd57
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2023-01-18 19:12:17 +0100

    Merge branch 'dev' into 12-initialize-data-classes + sample data connect with workaround + sample mockRepoUse in Prototyping.kt

--- app/src/main/java/com/example/weatherhook/data/repository/weatherHookRepo.kt
+++ app/src/main/java/com/example/weatherhook/data/repository/weatherHookRepo.kt
@@ -5,22 +5,123 @@ import com.example.weatherhook.data.models.Weather
 import com.example.weatherhook.data.models.WeatherHook
 import com.example.weatherhook.data.models.WeatherHookList
 import com.google.gson.Gson
-import java.io.File
-
 
 class WeatherHookRepo {
     fun loadAllData(): WeatherHookList{
         return try {
             val gson = Gson()
-            val jsonString = File("../data.json").readText()
-            Log.d("myTag", jsonString)
+            val jsonString = jsonData
             gson.fromJson(jsonString, WeatherHookList::class.java)
         } catch (e: Exception) {
             Log.e("DataRepository", "Error loading json data", e)
-            return WeatherHookList( listOf(WeatherHook(active = false, title = "Error", location = Pair(0.2f,0.3f), timeToEvent = 2, relevantDays = listOf("Monday"), triggers = listOf(
+            return WeatherHookList( listOf(WeatherHook(active = false, title = "Error", location = Pair(0.2f,0.3f), timeToEvent = 2, relevantDays = listOf("MO"), triggers = listOf(
                 Weather(weatherPhenomenon = 2, correspondingIntensity = 2)) )
             )
             )
         }
     }
+
+    private val jsonData = """{
+  "events": [
+    {
+      "active": false,
+      "title": "Swimming",
+      "location": {
+        "x": 50.0,
+        "y": 75.0
+      },
+      "timeToEvent": 3,
+      "relevantDays": [
+        "SA",
+        "SU"
+      ],
+      "triggers": [
+        {
+          "weatherPhenomenon": 0,
+          "correspondingIntensity": false
+        }
+      ]
+    },
+    {
+      "active": true,
+      "title": "Climbing",
+      "location": {
+        "x": 50.0,
+        "y": 75.0
+      },
+      "timeToEvent": 3,
+      "relevantDays": [
+        "MO",
+        "TU",
+        "WE",
+        "TH",
+        "FR"
+      ],
+      "triggers": [
+        {
+          "weatherPhenomenon": 1,
+          "correspondingIntensity": 0.5
+        },
+        {
+          "weatherPhenomenon": 5,
+          "correspondingIntensity": 18
+        }
+      ]
+    },
+    {
+      "active": true,
+      "title": "Surfing",
+      "location": {
+        "x": 50.0,
+        "y": 75.0
+      },
+      "timeToEvent": 5,
+      "relevantDays": [
+        "MO",
+        "TU",
+        "WE",
+        "TH",
+        "FR",
+        "SA",
+        "SU"
+      ],
+      "triggers": [
+        {
+          "weatherPhenomenon": 4,
+          "correspondingIntensity": 3
+        },
+        {
+          "weatherPhenomenon": 5,
+          "correspondingIntensity": 20
+        }
+      ]
+    },
+    {
+      "active": false,
+      "title": "Jogging",
+      "location": {
+        "x": 50.0,
+        "y": 75.0
+      },
+      "timeToEvent": 1,
+      "relevantDays": [
+        "MO",
+        "TU",
+        "WE",
+        "TH",
+        "FR"
+      ],
+      "triggers": [
+        {
+          "weatherPhenomenon": 1,
+          "correspondingIntensity": 0.7
+        },
+        {
+          "weatherPhenomenon": 2,
+          "correspondingIntensity": 0.33
+        }
+      ]
+    }
+  ]
+}"""
 }
