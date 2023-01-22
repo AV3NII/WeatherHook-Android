e9ba2ab026ad6a910ba708c18c0ab5495376947a
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2023-01-22 18:57:29 +0100

    cleared prototype screen

--- app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
+++ app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import com.example.weatherhook.data.repository.WeatherHookRepo
-import com.example.weatherhook.ui.components.WeatherEventList
 
 
 class Prototyping : Fragment() {
@@ -38,9 +36,7 @@ class Prototyping : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         composeView.setContent {
-            Column() {
-                WeatherEventList(data, context = requireContext())
-            }
+
 
             
         }
