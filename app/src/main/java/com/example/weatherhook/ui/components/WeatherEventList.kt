fb9e92209fb1b0a0c4b97de6ef591ffdda6b1b64
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2023-01-22 17:56:08 +0100

    component style matched to prototype

--- app/src/main/java/com/example/weatherhook/ui/components/WeatherEventList.kt
+++ app/src/main/java/com/example/weatherhook/ui/components/WeatherEventList.kt
@@ -1,20 +1,21 @@
 package com.example.weatherhook.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
-import androidx.compose.material.Surface
-import androidx.compose.material.Switch
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -28,19 +29,41 @@ import com.example.weatherhook.data.models.WeatherHookEventList
 fun WeatherHook(event: WeatherHookEvent) {
     var active = remember { mutableStateOf(event.active) }
 
+    var icon:Painter = painterResource(id = R.drawable.ic_baseline_anchor_24)
+    when(event.triggers[0].weatherPhenomenon){
+        0 -> {icon = painterResource(R.drawable.ic_baseline_settings_24)}
+        1 -> {icon = painterResource(R.drawable.ic_baseline_anchor_24)}
+        2 -> {icon = painterResource(R.drawable.ic_baseline_settings_24)}
+        3 -> {icon = painterResource(R.drawable.ic_baseline_settings_24)}
+        4 -> {icon = painterResource(R.drawable.ic_baseline_map_24)}
+        5 -> {icon = painterResource(R.drawable.ic_baseline_settings_24)}
+    }
 
     return Card(modifier = Modifier
         .padding(10.dp)
-        .height(120.dp), elevation = 5.dp, shape = RoundedCornerShape(15.dp)
+        .height(120.dp), elevation = 5.dp, shape = RoundedCornerShape(25.dp),
+
+        border = BorderStroke(1.5.dp, colorResource(id = R.color.black_green))
     ) {
-        Row {
-            Surface(shape = RoundedCornerShape(15.dp), modifier = Modifier
+        Row(modifier = Modifier.background(colorResource(id = R.color.light_green_bg))) {
+            Surface(shape = RoundedCornerShape(25.dp), modifier = Modifier
                 .height(120.dp)
-                .width(100.dp)) {
-                Box(modifier = Modifier.background(colorResource(id = R.color.mid_green))) {
+                .width(100.dp),
+                border = BorderStroke(1.5.dp, colorResource(id = R.color.black_green)))
+            {
+                Box(modifier = Modifier
+                    .background(colorResource(id = R.color.mid_green)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        modifier = Modifier.scale(3f),
+                        painter = icon,
+                        contentDescription = "Add Event",
+                        tint = colorResource(R.color.light_green)
+                    )
                 }
             }
-            Column(modifier = Modifier.padding(start = 10.dp)) {
+            Column(modifier = Modifier.padding(start = 5.dp)) {
                 Text(text = event.title, fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(start = 10.dp, bottom = 5.dp, top = 10.dp))
                 Text(text = "Time to event: ${event.timeToEvent} Day(s)", modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp))
                 Weekdays(event.relevantDays.split(";"), 20)
@@ -58,15 +81,20 @@ fun WeatherHook(event: WeatherHookEvent) {
 
 @Composable
 fun WeatherEventList(weatherHookEventList: WeatherHookEventList) {
-    Column(modifier = Modifier.fillMaxWidth().padding(end = Dp(20f))
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(end = Dp(20f), top = Dp(5f))
+        .padding(start = 10.dp, end = 10.dp)
         .verticalScroll(rememberScrollState())) {
+            weekDaysWidget(daysList = weatherHookEventList.events[0].relevantDays.split(";"))
+        
             weatherHookEventList.events.forEach{
 
                 WeatherHook(event = it)
 
             }
         
-        Box(modifier = Modifier.size(Dp(20f), Dp(65f)))
+        Box(modifier = Modifier.size(Dp(20f), Dp(55f)))
     }
 
 }
--- app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
+++ app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
@@ -4,21 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.example.weatherhook.data.repository.WeatherHookRepo
 import com.example.weatherhook.ui.components.WeatherEventList
-import com.example.weatherhook.ui.components.Weekdays
 
 
 class Prototyping : Fragment() {
@@ -48,26 +38,10 @@ class Prototyping : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         composeView.setContent {
-            Column(modifier = Modifier.fillMaxWidth(),horizontalAlignment = Alignment.CenterHorizontally) {
-
-                Card(modifier = Modifier
-                    .height(120.dp)
-                    .padding(start = Dp(0f), top = Dp(20f), end = Dp(20f), bottom = Dp(10f)),
-                    elevation = 5.dp,
-                    shape = RoundedCornerShape(15.dp)) {
-                    Column(modifier = Modifier.padding(start = Dp(15f), end = Dp(15f)), horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "Days of interest in the week", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                        Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                            Weekdays(listOfDays, 38)
-                        }
-                    }
-                }
-
+            Column() {
                 WeatherEventList(data)
-
-
-
             }
+
             
         }
     }
