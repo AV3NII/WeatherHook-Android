c15f604314a633def79210ee4579000f2bbe54ef
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2023-01-22 18:49:57 +0100

    created Home screen

---
+++ app/src/main/java/com/example/weatherhook/ui/screens/Home.kt
@@ -0,0 +1,49 @@
+package com.example.weatherhook.ui.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.weatherhook.data.repository.WeatherHookRepo
+import com.example.weatherhook.ui.components.WeatherEventList
+
+
+class Home : Fragment() {
+
+    private lateinit var composeView: ComposeView
+
+    val repo: WeatherHookRepo = WeatherHookRepo()
+    val data = repo.loadAllData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).also {
+            composeView = it
+        }
+    }
+
+    private val listOfDays = data.events[1].relevantDays.split(";")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        composeView.setContent {
+            Column() {
+                WeatherEventList(data, context = requireContext())
+            }
+
+            
+        }
+    }
+
+}
