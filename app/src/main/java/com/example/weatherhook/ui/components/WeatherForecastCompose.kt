43d1d6ed12d6e96bfa6ab9d112d0b5ec4b8285b2
DannyKuk <111462771+DannyKuk@users.noreply.github.com>
2023-01-25 12:51:46 +0100

    Idiot dont know how git works, oops

---
+++ app/src/main/java/com/example/weatherhook/ui/components/WeatherForecastCompose.kt
@@ -0,0 +1,9 @@
+package com.example.weatherhook.ui.components
+
+import androidx.compose.runtime.Composable
+import com.example.weatherhook.data.models.Weather
+
+@Composable
+fun WeatherForecast(listOfTemp: List<Weather>) {
+
+}
--- app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
+++ app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import com.example.weatherhook.data.models.Weather
 import com.example.weatherhook.data.repository.WeatherHookRepo
+import com.example.weatherhook.ui.components.WeatherForecast
 
 
 class Prototyping : Fragment() {
@@ -32,12 +34,22 @@ class Prototyping : Fragment() {
         }
     }
 
+    val listOfTemp = listOf<Weather>(
+        Weather(0, 14f),
+        Weather(1, 12f),
+        Weather(3, 9f),
+        Weather(5, 11f),
+        Weather(0, 20f),
+        Weather(0, 23f),
+        Weather(3, 8f),
+    )
+
     private val listOfDays = data.events[1].relevantDays.split(";")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         composeView.setContent {
 
-
+            WeatherForecast(listOfTemp)
             
         }
     }
