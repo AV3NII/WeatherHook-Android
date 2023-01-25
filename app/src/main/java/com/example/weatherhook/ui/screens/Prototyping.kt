6ed6b5d32030e967715a8b95afee44465e929c9a
DannyKuk <111462771+DannyKuk@users.noreply.github.com>
2023-01-25 21:50:18 +0100

    Resolve

--- app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
+++ app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
@@ -20,8 +20,6 @@ class Prototyping : Fragment() {
 
 
 
-
-
     private lateinit var composeView: ComposeView
 
     val repo: WeatherHookRepo = WeatherHookRepo()
@@ -47,7 +45,6 @@ class Prototyping : Fragment() {
 
 
     var reloadView = 1
-    //val listOfDays = data.relevantDays.split(";")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         composeView.setContent {
@@ -55,55 +52,13 @@ class Prototyping : Fragment() {
 
             var _reloadView by remember { mutableStateOf(reloadView) }
             var _triggerList by remember { mutableStateOf(data.triggers) }
-            //var triggerList = EventChooser().toMutableList()
+
             _triggerList = newHook(data)
             Log.d("shit", _triggerList.toString())
-            //Log.d("newTriggerList", triggerList.toString())
+
             //_reloadView +=1
 
 
-
-/*
-
-            Column(modifier = Modifier.fillMaxWidth()) {
-
-                Card(modifier = Modifier.padding(10.dp), elevation = 5.dp, shape = RoundedCornerShape(15.dp)) {
-                    Column(modifier = Modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "Days of interest in the week", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                        Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                            Weekdays(listOfDays, 38)
-                        }
-                    }
-                }
-
-
-
-
-                Card(modifier = Modifier
-                    .padding(10.dp)
-                    .height(120.dp), elevation = 5.dp, shape = RoundedCornerShape(15.dp)) {
-                    Row() {
-                        Surface(shape = RoundedCornerShape(15.dp), modifier = Modifier
-                            .height(120.dp)
-                            .width(100.dp)) {
-                            Box(modifier = Modifier.background(colorResource(id = R.color.mid_green))) {
-                            }
-                        }
-                        Column(modifier = Modifier.padding(start = 10.dp)) {
-                            Text(text = data.title, fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(start = 10.dp, bottom = 5.dp, top = 10.dp))
-                            Text(text = "Time to event: ${data.timeToEvent} Day(s)", modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp))
-                            Weekdays(listOfDays, 20)
-                        }
-                        Column(modifier = Modifier.padding(end = 20.dp)) {
-                                Switch(modifier = Modifier
-                                    .fillMaxSize()
-                                    .scale(1.3f), checked = data.active, onCheckedChange = {data.active = it})
-                        }
-
-
-                    }
-                }
-            }*/
 
 
 
