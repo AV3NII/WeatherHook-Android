9156d738521f97658e53683b1da10c4737e16674
DannyKuk <111462771+DannyKuk@users.noreply.github.com>
2023-02-10 15:52:17 +0100

    Add settings screen

---
+++ app/src/main/java/com/example/weatherhook/ui/components/DropdownMenuLanguage.kt
@@ -0,0 +1,10 @@
+package com.example.weatherhook.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DropDownMenuLanguage(onSelection: (Int) -> Unit) {
+
+
+
+}
---
+++ app/src/main/java/com/example/weatherhook/ui/screens/Settings.kt
@@ -0,0 +1,51 @@
+package com.example.weatherhook.ui.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.weatherhook.data.repository.WeatherHookRepo
+
+
+class Settings : Fragment() {
+
+    private lateinit var composeView: ComposeView
+
+    val repo: WeatherHookRepo = WeatherHookRepo()
+    val data = repo.loadAllData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).also {
+            composeView = it
+        }
+    }
+
+
+    private val listOfDays = data.events[1].relevantDays.split(";")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        composeView.setContent {
+
+            Column() {
+                Text(text = "Language")
+
+            }
+
+        }
+    }
+
+}
