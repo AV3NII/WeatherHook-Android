9f91cf66c6e096b1f547c7a033ac7183ae4b03a7
DannyKuk <111462771+DannyKuk@users.noreply.github.com>
2023-02-12 12:50:00 +0100

    API CALL - missing repo

--- app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
+++ app/src/main/java/com/example/weatherhook/ui/screens/Prototyping.kt
@@ -1,15 +1,12 @@
 package com.example.weatherhook.ui.screens
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.Text
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.coroutineScope
-import com.example.weatherhook.data.api.OpenWeatherApi
 import com.example.weatherhook.data.repository.WeatherHookRepo
 import kotlinx.coroutines.*
 import java.util.*
@@ -21,7 +18,6 @@ class Prototyping : Fragment() {
 
     val repo: WeatherHookRepo = WeatherHookRepo()
     val data = repo.loadAllData()
-    val apiService = OpenWeatherApi()
 
     
     @OptIn(DelicateCoroutinesApi::class)
@@ -40,11 +36,6 @@ class Prototyping : Fragment() {
     ): View {
 
 
-        lifecycle.coroutineScope.launch{
-            val ans = apiService.getWeatherForcast(48.215748f, 12.125494f, 7).await()
-            Log.d("shit",ans.toString())
-
-        }
         return ComposeView(requireContext()).also {
             composeView = it
 
